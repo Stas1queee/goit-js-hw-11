abda93535750369022c456fc2876596b4b9a5c7f
Stanislav <stas1que@ukr.net>
2023-02-11 15:59:54 +0100

    up

--- src/index.js
+++ src/index.js
@@ -4,19 +4,8 @@ import simpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import createMakrup from './markup';
 
-const refs = {
-    LINK: 'https://pixabay.com/api/',
-    API_KEY: '33394752-271cdc679e71c2d243d21eadd',
-    form: document.querySelector('form'),
-    input: document.querySelector('input'),
-    gallery: document.querySelector('.gallery'),
-    loadMore: document.querySelector('.load-more'),
-    btnText: document.querySelector('.text'),
 
-};
 
-async function fetchImages() {
 
-  } 
 
 
